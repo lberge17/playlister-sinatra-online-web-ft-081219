2709e835a47166af07833ed45da2cd76f764c3fe
Laura Berge <lauraeberge@gmail.com>
2019-10-01 17:15:39 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -42,7 +42,7 @@ class SongsController < ApplicationController
     @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
     @song.genre_ids = params[:genres]
     @song.save
-    binding.pry
+    
     flash[:message] = "Successfully updated song."
     redirect "/songs/#{@song.slug}"
   end
